06367bf99bae82cd88996a3d937c3ee92b9d2f63
gulsenpekcan <gulsenpekcan8@gmail.com>
2020-08-12 17:12:28 +0300

    transfer süresine bakıldı

--- data_transfering_with_spark.py
+++ data_transfering_with_spark.py
@@ -1,7 +1,8 @@
 
 from pyspark.sql import SparkSession
+import time
 
-spark = SparkSession \
+'''spark = SparkSession \
     .builder \
     .appName("Python Spark SQL example") \
     .config('spark.driver.extraClassPath', '/opt/spark/jars/postgresql-42.2.14.jar') \
@@ -10,5 +11,24 @@ spark = SparkSession \
 df = spark.read.jdbc('jdbc:postgresql://localhost:5432/project1', 'public.table1', properties={"user": "postgres", "password": "852456"})
 
 #Save the dataframe to the table.
-df.write.jdbc('jdbc:postgresql://localhost:5432/project1', 'public.table2', mode='overwrite', properties={"user": "postgres", "password": "852456"})
+df.write.jdbc('jdbc:postgresql://localhost:5432/project1', 'public.table2', mode='overwrite', properties={"user": "postgres", "password": "852456"})'''
 
+
+start = time.time()
+
+spark = SparkSession \
+        .builder \
+        .appName("Transfer data with spark") \
+        .config('spark.driver.extraClassPath', '/opt/spark/jars/postgresql-42.2.14.jar') \
+        .getOrCreate()
+
+df = spark.read.jdbc('jdbc:postgresql://localhost:5432/project1', 'public.data_table',
+                         properties={"user": "postgres", "password": "852456"})
+
+# Save the dataframe to the table.
+df.write.jdbc('jdbc:postgresql://localhost:5432/project1', 'public.transferred_data', mode='overwrite',
+                  properties={"user": "postgres", "password": "852456"})
+
+print("Execution time: %f" % (time.time() - start))
+
+# Execution time: 23.322607
---
+++ transferring_data.py
@@ -0,0 +1,66 @@
+
+import string
+import random
+import psycopg2
+
+DB_NAME = "project1"
+DB_USER = "postgres"
+DB_PASS = "852456"
+DB_HOST = "localhost"
+DB_PORT = "5432"
+
+
+def rand_data_generator(size=6, chars=string.ascii_letters + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+def generate_and_insert_data():
+    commands = (
+        """
+        CREATE TABLE data_table (
+            id serial PRIMARY KEY,
+            data VARCHAR(255) NOT NULL
+        )
+        """)
+
+    sql = "INSERT INTO data_table (data) VALUES (%s)"
+    data_list = []
+
+    conn = None
+    try:
+        conn = psycopg2.connect(database=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT)
+        print("Database connected successfully")
+
+        '''cur = conn.cursor()
+        # create table one by one
+        cur.execute(commands)
+
+        cur.close()
+        # commit the changes
+        conn.commit()
+        print("Table(s) created successfully")'''
+
+        cur = conn.cursor()
+
+        for _ in range(1000000):
+            rand_data = rand_data_generator()
+            cur.execute(sql, (rand_data,))
+
+        # commit the changes to the database
+        conn.commit()
+        print("Data inserted successfully")
+        # close communication with the PostgreSQL database server
+        cur.close()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+if __name__ == '__main__':
+    generate_and_insert_data()
+
+
+
